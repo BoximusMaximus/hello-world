bd8f7f8ca9b0d7ac9030463470918c8d2ed9d99c
abdelrahmansandresy <abdelrahmansandresy@gmail.com>
2026-06-02 16:23:19 -0400

    fixed the bug and added more features

--- new-feature.py
+++ new-feature.py
@@ -1,1 +1,4 @@
-print("This is the new feature.")``
+print("This is the new feature.")
+print("It has been added to the project.")
+print("This feature will enhance the user experience.")
+print("We are excited to share this with you!")
